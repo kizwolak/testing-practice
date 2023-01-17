66c4b73961a33e5d5b13ec7ea0abb1571ca0aabe
kizwolak <ki.zwolak@gmail.com>
2023-01-17 05:47:02 +0100

    Add analyseArray and Caesar's cipher

--- practice.js
+++ practice.js
@@ -24,4 +24,49 @@ export const calculator = {
     }
 }
 
-console.log(calculator.add(2, 2));
+export function caesarCipher(string, shift) {
+    let toBeReturned = '';
+    for (let i = 0; i < string.length; i++) {
+        let charCode = string.charCodeAt(i);
+        let shiftedCharCode = charCode + shift;
+        if (65 <= charCode && charCode <= 90) {
+            if (shiftedCharCode > 90) {
+                let actualShift = (shiftedCharCode) - 91;
+                let actualLetter = 65 + actualShift;
+                toBeReturned += String.fromCharCode(actualLetter);
+            } else {
+                toBeReturned += String.fromCharCode(shiftedCharCode);
+            }
+        } else if (97 <= charCode && charCode <= 122) {
+            if (shiftedCharCode > 122) {
+                let actualShift = (shiftedCharCode) - 123;
+                let actualLetter = 97 + actualShift;
+                toBeReturned += String.fromCharCode(actualLetter);
+            } else {
+                toBeReturned += String.fromCharCode(shiftedCharCode);
+            }
+        }
+    }
+    return toBeReturned;
+}
+
+export function analyseArray (array) {
+    const average = function (array) {
+        let total = 0;
+        for (const number of array) {
+            total += number;
+        }
+        return total/array.length;
+    };
+    const calculatedAverage = average(array);
+    const min = Math.min(...array);
+    const max = Math.max(...array);
+    const length = array.length;
+    return {
+        average: calculatedAverage,
+        min: min,
+        max: max,
+        length: length,
+    }
+}
+
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-import {capitalise, reverseString, calculator} from './practice.js';
+import {capitalise, reverseString, calculator, caesarCipher, analyseArray} from './practice.js';
 
 test('return capitalised string', () => {
     expect(capitalise('hello')).toBe('Hello');
@@ -16,3 +16,14 @@ test('perform all basic operations', () => {
     expect(calculator.multiply(2, 2)).toBe(4);
 });
 
+test('shift everything perfectly!', () => {
+    expect(caesarCipher('Zztest', 1)).toBe('Aauftu');
+});
+
+test('output the right array values', () => {
+    const testAnalysis = analyseArray([2, 1, 3, 2]);
+    expect(testAnalysis.average).toBe(2);
+    expect(testAnalysis.min).toBe(1);
+    expect(testAnalysis.max).toBe(3);
+    expect(testAnalysis.length).toBe(4);
+});
